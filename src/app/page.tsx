9346dc1de2dc7086255f64fa4d8be9febf7bae6e
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-05-28 19:25:37 -0300

    feat: Add utility functions and button component with styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       teste
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        teste
+        <Button>
+          teste
+        </Button>
       </div>
     </div>
   );
