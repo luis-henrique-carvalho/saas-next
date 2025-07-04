c486a9874ce34ecb847d3984dee8118e3b8a771a
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-07-04 01:05:43 -0300

    feat: Add appointment chart component to dashboard for visualizing daily appointments and revenue

--- src/app/(core)/dashboard/page.tsx
+++ src/app/(core)/dashboard/page.tsx
@@ -16,6 +16,7 @@ import {
 import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 
+import AppointmentChart from "./components/chart/appointment-chart";
 import { DatePicker } from "./components/date-picker";
 import StatsCards from "./components/status-card";
 
@@ -50,11 +51,16 @@ const Dashboard = async ({ searchParams }: DashboardPageProps) => {
   const fromDate = new Date(from)
   const toDate = new Date(to)
 
+  const chartStarDate = dayjs().subtract(10, "day").startOf("day").toDate();
+  const chartEndDate = dayjs().add(10, "day").endOf("day").toDate();
+
   const [
     totalRevenueResult,
     totalAppointmentsResult,
     totalPatientsResult,
     totalDoctorsResult,
+    dailyAppointmentsDataRaw,
+    topDoctorsRaw
   ] = await Promise.all([
     prisma.appointment.aggregate({
       _sum: { priceInCents: true },
@@ -88,15 +94,50 @@ const Dashboard = async ({ searchParams }: DashboardPageProps) => {
         clinicId: session.user.clinic.id,
       },
     }),
+    prisma.$queryRaw<
+      {
+        date: Date;
+        appointments: number;
+        revenue: number;
+      }[]>
+      `SELECT
+        DATE_TRUNC('day', "date") AS "date",
+        COUNT(id) as "appointments",
+        SUM("priceInCents") as "revenue"
+      FROM "appointment"
+      WHERE
+        "clinicId" = ${session.user.clinic.id}
+        AND "date" >= ${chartStarDate}
+        AND "date" <= ${chartEndDate}
+      GROUP BY
+        DATE_TRUNC('day', "date")
+      ORDER BY
+        "date" ASC`,
+    prisma.$queryRaw<
+      { doctorId: string; doctorName: string; appointments: number }[]
+    >`
+    SELECT
+      d.id AS "doctorId",
+      d.name AS "doctorName",
+      COUNT(a.id) AS "appointments"
+    FROM "appointment" a
+    JOIN "doctor" d ON d.id = a."doctorId"
+    WHERE a."clinicId" = ${session.user.clinic.id}
+    GROUP BY d.id, d.name
+    ORDER BY "appointments" ASC
+    LIMIT 5
+  `
   ]);
 
-  console.log("Total Revenue Result:", totalRevenueResult);
-  console.log("Total Appointments Result:", totalAppointmentsResult);
-  console.log("Total Patients Result:", totalPatientsResult);
-  console.log("Total Doctors Result:", totalDoctorsResult);
+  console.log("Top Doctors Raw Data:", topDoctorsRaw);
+
+  const dailyAppointmentsData = dailyAppointmentsDataRaw.map((item) => ({
+    date: dayjs(item.date).format("YYYY-MM-DD"),
+    appointments: Number(item.appointments),
+    revenue: Number(item.revenue),
+  }));
 
   const totalAppointment = totalRevenueResult._sum.priceInCents
-
 
   return (
     <PageContainer>
@@ -116,6 +157,12 @@ const Dashboard = async ({ searchParams }: DashboardPageProps) => {
           totalPatients={totalPatientsResult._count}
           totalDoctors={totalDoctorsResult._count}
         />
+
+        <div className="grid grid-cols-[2.25fr_1fr] gap-4">
+          <AppointmentChart
+            dailyAppointmentsData={dailyAppointmentsData}
+          />
+        </div>
       </PageContent>
     </PageContainer>
   );
